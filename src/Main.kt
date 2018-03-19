ea34781fd7b8c40aeb5685a556f41a31ce49305e
Jairo <jairoandre@gmail.com>
2018-03-19 01:53:31 -0300

    New stuffs

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,8 @@ class Point(val x: Double, val y: Double) {
     override fun toString() = "($x, $y)"
 }
 
+class Vector(val x: Double, val y: Double)
+
 class Line(val a: Point, val b: Point) {
     fun isPlane() = a.y == b.y
     override fun toString() = "a: $a | b: $b"
@@ -17,6 +19,37 @@ class Surface(val points: List<Point>) {
         lines = points.drop(1).dropLast(2).mapIndexed { index, point -> Line(point, points[index]) }
         plane = lines.first { it.isPlane() }
     }
+
+    fun inside(point: Point) : Boolean {
+        val polygon = listOf(listOf(Point(0.0, 0.0)), points, listOf(Point(6999.0, 0.0))).flatten()
+        var inside = false
+        var i = 0
+        var j = polygon.size - 1
+        while (i < polygon.size) {
+            val iPoint = polygon[i]
+            val jPoint = polygon[j]
+            val intersect = iPoint.y > point.y != jPoint.y > point.y && point.x < (jPoint.x - iPoint.x) * (point.y - iPoint.y) / (jPoint.y - iPoint.y) + iPoint.x
+            if (intersect) inside = !inside
+            j = i++
+        }
+        return inside
+    }
+}
+
+class Lander(input: Scanner) {
+    val position: Point
+    val velocity: Vector
+    val fuel: Int
+    val rotate: Int
+    val power: Int
+
+    init {
+        position = Point(input.nextDouble(), input.nextDouble())
+        velocity = Vector(input.nextDouble(), input.nextDouble())
+        fuel = input.nextInt()
+        rotate = input.nextInt()
+        power = input.nextInt()
+    }
 }
 
 fun debug(msg: String) = System.err.print(msg)
@@ -29,23 +62,24 @@ fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     val surfaceN = input.nextInt() // the number of points used to draw the surface of Mars.
     val surface = Surface((0 until surfaceN).map { Point(input.nextDouble(), input.nextDouble()) })
+
     debug(surface.plane.toString())
 
     // game loop
     while (true) {
-        val X = input.nextInt()
-        val Y = input.nextInt()
-        val hSpeed = input.nextInt() // the horizontal speed (in m/s), can be negative.
-        val vSpeed = input.nextInt() // the vertical speed (in m/s), can be negative.
-        val fuel = input.nextInt() // the quantity of remaining fuel in liters.
-        val rotate = input.nextInt() // the rotation angle in degrees (-90 to 90).
-        val power = input.nextInt() // the thrust power (0 to 4).
-
-        // Write an action using println()
-        // To debug: System.err.println("Debug messages...");
-
-
+        val lander = Lander(input)
         // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
         println("-20 3")
     }
 }
+
+// Extensions
+
+fun Double.randomInt(min: Int, max: Int) = min + (max * this)
+
+// Gene
+
+class Gene(
+    val power: Int = Math.random().randomInt(0, 4)
+
+)
